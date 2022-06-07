158ad5e96ec34055138be4dc2ccd416ddcf8927c
Sabrina Gadaleta <sabrinagadaleta@gmail.com>
2022-06-07 15:53:39 +0200

    installation jsonwebtoken

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
 const app = express();
-
 const mongoose = require('mongoose');
+const sauceRoutes = require('./routes/sauce');
+const userRoutes = require('./routes/user');
+
 
 mongoose.connect('mongodb+srv://Sabrina:zazzino2008@cluster0.d9xe2.mongodb.net/?retryWrites=true&w=majority',
   {
@@ -12,30 +13,18 @@ mongoose.connect('mongodb+srv://Sabrina:zazzino2008@cluster0.d9xe2.mongodb.net/?
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+app.use(express.json());
 
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
+app.use('/api/sauces', sauceRoutes);
+app.use('/api/auth', userRoutes);
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
 
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
 
 module.exports = app;
