f4da7c1d774f1228c6187af096e05ffec2bf7bcb
Sabrina Gadaleta <sabrinagadaleta@gmail.com>
2022-06-07 15:29:55 +0200

    installation bcrypt

---
+++ backend/routes/user.js
@@ -0,0 +1,11 @@
+// Création de la route permettant les actions liées à "user"
+
+const express = require('express');
+const router = express.Router();
+
+const userCtrl = require('../controllers/user');
+
+router.post('/signup', userCtrl.signup);
+router.post('/login', userCtrl.login);
+
+module.exports = router;
