9031b9334d42ea34aadfe0a67edb5d4ae90f13a1
Sabrina Gadaleta <sabrinagadaleta@gmail.com>
2022-06-14 11:15:01 +0200

    corrections

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -31,10 +31,10 @@ exports.createSauce = async (req, res) => {
 }
 
 // Récupération des informations de toutes les sauces
-exports.getAllSauce = async (req, res, next) => {
+exports.getAllSauce = async (req, res) => {
     try {
-        await Sauce.find();
-        return res.status(200).json(Sauce);
+        let sauces = await Sauce.find();
+        return res.status(200).json(sauces);
     }
     catch (err) {
         console.error(err);
@@ -43,10 +43,10 @@ exports.getAllSauce = async (req, res, next) => {
 };
 
 // Récupération des informations d'une seule sauce
-exports.getOneSauce = async (req, res, next) => {
+exports.getOneSauce = async (req, res) => {
     try {
-        await Sauce.findOne({ _id: req.params.id, });
-        return res.status(200).json(Sauce)
+        let sauce = await Sauce.findOne({ _id: req.params.id });
+        return res.status(200).json(sauce)
     }
     catch (err) {
         console.error(err);
@@ -57,38 +57,41 @@ exports.getOneSauce = async (req, res, next) => {
 // Modification des informations d'une seule sauce
 exports.modifySauce = async (req, res) => {
     try {
-        let sauceObject = req.file ?
-            {
-                ...JSON.parse(req.body.sauce),
-                imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-            }
-            : { ...req.body };
-        await Sauce.updateOne({ _id: req.params.id }, {
-            _id: req.params.id,
-            userId: sauceObject.userId,
-            name: sauceObject.name,
-            manufacturer: sauceObject.manufacturer,
-            description: sauceObject.description,
-            mainPepper: sauceObject.mainPepper,
-            imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
-            heat: sauceObject.heat,
+      let sauceObject = req.body.sauce ? JSON.parse(req.body.sauce) : req.body;
+  
+      if (req.file) {
+        let sauce = await Sauce.findOne({ _id: req.params.id });
+        fs.unlink(`images/${sauce.imageUrl.split('/images/')[1]}`, async () => {
+          sauceObject.imageUrl = `${req.protocol}://${req.get('host')}/images/${req.file.filename}`;
+          await Sauce.updateOne({ _id: req.params.id }, {
+            imageUrl: sauceObject.imageUrl
+          });
         });
-        return res.status(200).json({ message: "Sauce modifiée!" })
+      }
+      await Sauce.updateOne({ _id: req.params.id }, {
+        userId: sauceObject.userId,
+        name: sauceObject.name,
+        manufacturer: sauceObject.manufacturer,
+        description: sauceObject.description,
+        mainPepper: sauceObject.mainPepper,
+        heat: sauceObject.heat
+      });
+      return res.status(200).json({ message: 'Sauce modifiée !' });
     }
-    catch (err) {
-        console.error(err);
-        return res.status(500).send("Internal error");
+    catch (error) {
+      console.error(error);
+      return res.status(500).json({ message: 'Erreur interne !' });
     }
-
-};
+  };
+  
 
 // Suppression d'une seule sauce 
-exports.deleteSauce = async (req, res, next) => {
+exports.deleteSauce = async (req, res) => {
     try {
-        let sauce = Sauce.findOne({ _id: req.params.id })
+        let sauce = await Sauce.findOne({ _id: req.params.id })
         const filename = sauce.imageUrl.split("/images/")[1];
         fs.unlink(`images/${filename}`, async () => {
-            await Sauce.deleteOne({ _id: req.params.id });
+            await sauce.deleteOne({ _id: req.params.id });
             return res.status(200).json({ message: "Sauce supprimée !" });
         });
     }
@@ -100,15 +103,63 @@ exports.deleteSauce = async (req, res, next) => {
 
 // Ajout des likes et dislikes pour chaque sauce
 
-exports.likeSauce = (req, res, next) => {
-    /* Si le client Like cette sauce */
-    if (req.body.like === 1) {
+exports.likeSauce = async (req, res) => {
+    try {
+        let sauce = await Sauce.findOne({ _id: req.params.id });
+        if (!sauce) {
+            throw res.status(404).json({ message: 'Sauce introuvable !' });
+        }
+        switch (req.body.like) {
+            /* Si le client Like cette sauce */
+            case 1:
+                if (!sauce.usersLiked.includes(req.body.userId)) {
+                    await Sauce.updateOne(
+                        { _id: req.params.id },
+                        {
+                            $inc: { likes: 1 },
+                            $push: { usersLiked: req.body.userId }
+                        }
+                    )
+                }
+                break;
+            /* Si le client disike cette sauce */
+            case -1:
+                if (!sauce.usersDisliked.includes(req.body.userId)) {
+                    await Sauce.updateOne(
+                        { _id: req.params.id },
+                        {
+                            $inc: { dislikes: 1 },
+                            $push: { usersDisliked: req.body.userId }
+                        }
+                    )
+                }
+                break;
+            /* Si le client annule son choix */
+            case 0:
+                if (sauce.usersLiked.includes(req.body.userId)) {
+                    await Sauce.updateOne(
+                        { _id: req.params.id },
+                        {
+                            $inc: { likes: -1 },
+                            $pull: { usersLiked: req.body.userId }
+                        }
+                    )
+                }
+                if (sauce.usersDisliked.includes(req.body.userId)) {
+                    await Sauce.updateOne(
+                        { _id: req.params.id },
+                        {
+                            $inc: { dislikes: -1 },
+                            $pull: { usersDisliked: req.body.userId }
+                        }
+                    )
+                }
+                break;
+        }
+        return res.status(200).json({ message: 'Like ou Dislike mis à jour !' });
     }
-    /* Si le client disike cette sauce */
- else if (req.body.like === -1) {
-}
-/* Si le client annule son choix */
-else {
-
-}
-}
+    catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Erreur interne !' });
+    }
+};
